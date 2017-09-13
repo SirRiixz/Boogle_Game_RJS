b5d85dcf2d96555f28a31102eaec9e7fd1f478c8
Richard <r.santos92@live.com>
2017-09-13 14:26:04 +1000

    Boggle working with random alphabet letters added

--- boggle_board.rb
+++ boggle_board.rb
@@ -9,24 +9,25 @@ class BoggleBoard
   def shake!
     @space.map! do | space |
       space = @alphabet.shuffle.take(1)
+    @q
     end
 
-  end
 
-    #take spaces
-    #replace item with random letters(prob good idea to create a var for this)
-    #split it into our board
-    #call the shake! method without BoggleBoard class
-    #could use MAP
-    # output_string = String.new
-    # second_array = @alphabet
-    # 4.times do
-    # output_string << second_array.sort_by { rand }.shift(4).join(' ') + ("\n")
-    #
-    # end
-    # output_string
-    #
-    # end
+
+    # take spaces
+    # replace item with random letters(prob good idea to create a var for this)
+    # split it into our board
+    # call the shake! method without BoggleBoard class
+    # could use MAP
+    output_string = String.new
+    second_array = @alphabet
+    4.times do
+    output_string << second_array.sort_by { rand }.shift(4).join(' ') + ("\n")
+
+    end
+    output_string
+
+    end
 
   #Defining to_s on an object controls how the object is
   #represented as a string, e.g., when you pass it to puts
@@ -34,6 +35,7 @@ class BoggleBoard
     #a new string for the output
     output_string = String.new
     letter_array = @space
+
     4.times do
       output_string << letter_array.shift(4).join(' ') + ("\n")
 
@@ -55,3 +57,6 @@ puts board.to_s
 
 board = BoggleBoard.new
 puts board.shake!
+
+board = BoggleBoard.new
+puts
